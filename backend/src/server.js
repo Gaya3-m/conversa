1adce7e85916cc60d5f3294f9c2c8599a566012d
Gaya3-m <gayathri2609z@gmail.com>
2025-08-20 18:08:03 +0530

    axios tanstack query setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
@@ -13,6 +14,10 @@ dotenv.config();
 const app = express();
 const PORT=process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true, // Allow cookies to be sent with requests
+}))
 app.use(express.json());
 app.use(cookieParser());
 
