4aca120ae19110e54579e6daf60715abccfe7b43
Gaya3-m <gayathri2609z@gmail.com>
2025-08-25 15:39:45 +0530

    login page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { GhostIcon } from "lucide-react";
 import { Link } from "react-router";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { signup } from "../lib/api.js";
+import useSignup from "../hooks/useSignup.js";
 
 const SignUpPage = () => {
   const [signupData, setSignupData] = useState({
@@ -11,13 +10,17 @@ const SignUpPage = () => {
     password: "",
   });
 
-  const queryClient=useQueryClient();
-
-  const {mutate: signupMutation, isPending, error}=useMutation({
-    mutationFn: signup,
-
-    onSuccess:()=>queryClient.invalidateQueries({queryKey: ["authUser"]})
-  })
+  // const queryClient = useQueryClient();
+  // const {
+  //   mutate: signupMutation,
+  //   isPending,
+  //   error,
+  // } = useMutation({
+  //   mutationFn: signup,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+  
+  const {isPending, error, signupMutation}=useSignup();
 
   const handleSignup = (e) => {
     e.preventDefault();
